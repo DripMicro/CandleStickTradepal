b42fd0fb81d8e6b5eb87b73e442fb30ff4fc9d86
Full Stack || Blockchain Eng (Techking5287) <91337512+Techking5287@users.noreply.github.com>
2023-09-25 02:16:31 +0900

    updated

--- config/config(db).js
+++ config/config(db).js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/CandleStick_TradePal").then(() => {
-    console.log("Database connected!");
+mongoose.connect("mongodb://localhost:27017/CandleStick_TradePal", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Database connected!");
 }).catch((err) => {
-    console.log(err, " error");
-})                  
+  console.log(err, "error");
+});
